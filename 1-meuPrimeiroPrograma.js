577fe90136cc0326693c675bf74d417b1d4c384f
LAPTOP-UR8TUAM2\Aline Reis <rafabrat@hotmail.com>
2020-10-07 19:07:22 -0300

    Meu Primeiro Commmit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Meu Primeiro Programa: trabalhando com variáveis");
+
+const IDADE = 26;
+ 
+console.log(IDADE);
+console.log(IDADE+2);
+console.log(IDADE-2);
+console.log(IDADE*2);
+console.log(IDADE/2);
+
+
