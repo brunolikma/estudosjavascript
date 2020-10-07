a3e2d063f025309b9f5dc78c6dccdb6603210351
LAPTOP-UR8TUAM2\Aline Reis <rafabrat@hotmail.com>
2020-10-07 19:12:19 -0300

    Aulas Alura

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com Variaveis");
+
+const idade = 29;
+const nome = "Bruno";
+
+console.log(idade);
+
+//nao fazer
+//mes = "Janeiro";
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,13 @@
+console.log ("Trabalhando com atribuicao de variaveis");
+
+const idade = 29;
+let nome = "Bruno";
+const sobrenome = "Lima";
+
+console.log (nome, sobrenome);
+console.log (nome + " " + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nomeCompleto = nome + sobrenome;
+
+console.log (nome);
